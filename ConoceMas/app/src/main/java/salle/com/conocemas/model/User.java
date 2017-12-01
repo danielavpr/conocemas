e62376bdccbca891330ee4477867532f7e9fde85
Daniela R <daniela.ramon12@gmail.com>
2017-11-30 21:44:52 -0600

    Log in and log out. Issue in navigation

---
+++ ConoceMas/app/src/main/java/salle/com/conocemas/model/User.java
@@ -0,0 +1,14 @@
+package salle.com.conocemas.model;
+
+import android.media.Image;
+import android.net.Uri;
+
+/**
+ * Created by ramon on 30/11/17.
+ */
+
+public class User {
+    public String username;
+    public String useremail;
+    public Uri userphoto;
+}
